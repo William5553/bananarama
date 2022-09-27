5f8a48017b1cdae1d3fab3c2a4f8af5fe3a458ae
William Kang <william@williamk.ga>
2022-09-27 10:15:28 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 let x = 2;
 let y = 2;
 
-input.onButtonPressed(Button.A, () => {
-    x--;
+const updateLED = () => {
     if (x < 0)
         x = 4;
+    if (x > 4)
+        x = 0;
+    basic.clearScreen();
+    led.plot(x, y);
+};
+
+input.onButtonPressed(Button.A, () => {
+    x--;
     updateLED();
 });
 
 input.onButtonPressed(Button.B, () => {
     x++;
-    if (x > 4)
-        x = 0;
     updateLED();
 });
-
-const updateLED = () => {
-    basic.clearScreen();
-    led.plot(x, y);
-};
