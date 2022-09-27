22a42c4adf320e38de02af16f529a6b3a630b0d2
William Kang <william@williamk.ga>
2022-09-27 10:32:41 -0400

    legoooo

--- main.ts
+++ main.ts
@@ -1,21 +1,39 @@
-let x = 2;
-let y = 2;
+let ledX = 2;
+let ledY = 2;
+// x = true, y = false
+let changingX = true;
 
 const updateLED = () => {
-    if (x < 0)
-        x = 4;
-    if (x > 4)
-        x = 0;
+    if (ledX < 0)
+        ledX = 4;
+    if (ledX > 4)
+        ledX = 0;
+    if (ledY < 0)
+        ledY = 4;
+    if (ledY > 4)
+        ledY = 0;
     basic.clearScreen();
-    led.plot(x, y);
+    led.plot(ledX, ledY);
 };
 
+updateLED();
+
 input.onButtonPressed(Button.A, () => {
-    x--;
+    if (changingX)
+        ledX--;
+    else
+        ledY--;
     updateLED();
 });
 
 input.onButtonPressed(Button.B, () => {
-    x++;
+    if (changingX)
+        ledX++;
+    else
+        ledY++;
     updateLED();
+});
+
+input.onLogoEvent(TouchButtonEvent.Pressed, () => {
+    changingX = !changingX;
 });
