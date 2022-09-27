ff0316928bb3d88b518d540390868041c8bbae32
William Kang <william@williamk.ga>
2022-09-27 10:36:32 -0400

    running thru the 6 with my woes

--- main.ts
+++ main.ts
@@ -4,14 +4,6 @@ let ledY = 2;
 let changingX = true;
 
 const updateLED = () => {
-    if (ledX < 0)
-        ledX = 4;
-    if (ledX > 4)
-        ledX = 0;
-    if (ledY < 0)
-        ledY = 4;
-    if (ledY > 4)
-        ledY = 0;
     basic.clearScreen();
     led.plot(ledX, ledY);
 };
@@ -19,17 +11,17 @@ const updateLED = () => {
 updateLED();
 
 input.onButtonPressed(Button.A, () => {
-    if (changingX)
+    if (changingX && ledX > 0) {
         ledX--;
-    else
+    } else if (ledY > 0 && !changingX)
         ledY--;
     updateLED();
 });
 
 input.onButtonPressed(Button.B, () => {
-    if (changingX)
+    if (changingX && ledX < 4)
         ledX++;
-    else
+    else if (!changingX && ledY < 4)
         ledY++;
     updateLED();
 });
