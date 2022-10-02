ead4a1dd1f9d4eb905370465fe3033aa1c090f22
William Kang <contact@williamk.ga>
2022-10-01 22:40:34 -0400

    don't need those

--- main.py
+++
@@ -1,7 +0,0 @@
-def on_button_pressed_a():
-    basic.show_string('William')
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    basic.show_string('Hello')
-input.on_button_pressed(Button.B, on_button_pressed_b)
